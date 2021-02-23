46f7fcb992d6330d9caa7645648b3a6579eb4e48
Engin Yenice <enginyenice2626@gmail.com>
2021-02-24 01:09:34 +0300

    UseSqlServer name path

--- DataAccess/Concrete/EntityFramework/ReCapProjectContext.cs
+++ DataAccess/Concrete/EntityFramework/ReCapProjectContext.cs
@@ -10,7 +10,7 @@ namespace DataAccess.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ReCapProject;Integrated Security=True;");
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ReCapProject;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             base.OnConfiguring(optionsBuilder);
         }
 
