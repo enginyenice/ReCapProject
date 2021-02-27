5f1caeccb9eaa598f85fe7ae9e59fb129c3676c7
Engin Yenice <enginyenice2626@gmail.com>
2021-02-27 21:07:15 +0300

    Path edited.

--- Business/Constants/DefaultNameOrPath.cs
+++ Business/Constants/DefaultNameOrPath.cs
@@ -5,10 +5,9 @@ namespace Business.Constants
 {
     public static class DefaultNameOrPath
     {
-        public static string WWWROOT = "wwwroot/";
-        public static string ImageDirectory = WWWROOT + "images"; // Klasör adı
-        public static string FileDirectory = WWWROOT + "/files"; // Klasör Adı
-        public static string TestDirectory = WWWROOT + "/images/sub1/sub2/sub3/sub4/sub5/sub6/sub7/sub8/sub9"; // Test Path adı
+        public static string ImageDirectory = "images"; // Klasör adı
+        public static string FileDirectory = "/files"; // Klasör Adı
+        public static string TestDirectory = "/images/sub1/sub2/sub3/sub4/sub5/sub6/sub7/sub8/sub9"; // Test Path adı
         public static string NoImagePath = ImageDirectory + "/NoImage.jpg"; // Sabit resim yolu
     }
 }
